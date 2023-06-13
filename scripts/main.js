79b83ee9d56797eda99b450f92fa20b6a8c03d85
João Agostinho <63238478+KrOOb-5@users.noreply.github.com>
2023-06-13 16:48:27 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+//image switcher code
+
+let myImage = document.querySelector("img");
+
+
+myImage.onclick = function (){
+    let mySrc = myImage.getAttribute("src");
+    if( mySrc === "images/firefox-icon.png"){
+        myImage.setAttribute("src", "images/firefox2.png");
+    }else{
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+}
+
+//Personalized welcomed messages
+
+let myHeading = document.querySelector("h1");
+let myButton = document.querySelector("button");
+
+function setUserName(){
+    let myName = prompt("Enter your name");
+    if(!myName){
+        setUserName();
+    }else{
+        localStorage.setItem("name", myName);
+        myHeading.innerHTML = "Mozilla is cool, " + myName;
+    }
+}
+
+if(!localStorage.getItem("name")){
+    setUserName();
+}else{
+    let storedName = localStorage.getItem("name");
+    myHeading.innerHTML = "Mozilla is cool, " + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+  }
